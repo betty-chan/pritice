df64d676ec3acbba351985792b3d1ae37ae34425
chenxin <chenxin@starlinke.com>
2021-04-14 18:34:28 +0800

    init

--- promise2.js
+++ promise2.js
@@ -19,19 +19,22 @@ Promise.then((data)=>{},(error)=>{})//参数可选
 const PENDING = "pending";
 const RESOLVED = "resolved";
 const REJECTED = "rejected";
-class MyPromise{
-  constructor(fn){
+class MyPromise {
+  constructor(fn) {
     this.currentState = PENDING;
     this.value = undefined;
     this.resolvedCallbacks = [];
     this.rejectedCallbacks = [];
+    this.resolve = this.resolve.bind(this);
+    this.reject = this.reject.bind(this);
     try {
+      fn = fn.bind(this);
       fn(this.resolve, this.reject);
     } catch (error) {
       this.reject(error);
     }
   }
-  resolve(value){
+  resolve(value) {
     // 如果 value 是个 Promise，递归执行
     if (value instanceof MyPromise) {
       return value.then(this.resolve, this.reject)
@@ -39,9 +42,9 @@ class MyPromise{
     // 异步执行，保证执行顺序
     setTimeout(() => {
       if (this.currentState === PENDING) {
-          this.currentState = RESOLVED;
-          this.value = value;
-          this.resolvedCallbacks.forEach((item) =>{ return item()});
+        this.currentState = RESOLVED;
+        this.value = value;
+        this.resolvedCallbacks.forEach((item) => { return item() });
       }
     })
   }
@@ -51,21 +54,21 @@ class MyPromise{
       if (this.currentState === PENDING) {
         this.currentState = REJECTED;
         this.value = error;
-        this.rejectedCallbacks.forEach((item) => {return item()});
+        this.rejectedCallbacks.forEach((item) => { return item() });
       }
     })
   }
-  then(onResolved, onRejected){
+  then(onResolved, onRejected) {
     var self = this;
     var promise2;
-    onResolved = typeof onResolved === 'function' ? onResolved : (v) => {return v};
-    onRejected = typeof onRejected === 'function' ? onRejected : (r) => {throw r};
+    onResolved = typeof onResolved === 'function' ? onResolved : (v) => { return v };
+    onRejected = typeof onRejected === 'function' ? onRejected : (r) => { throw r };
     if (self.currentState === RESOLVED) {
       return (promise2 = new MyPromise(function (resolve, reject) {
         setTimeout(function () {
           try {
             var x = onResolved(self.value);
-            resolutionProcedure(promise2, x, resolve, reject);
+            this.resolutionProcedure(promise2, x, resolve, reject);
           } catch (reason) {
             reject(reason);
           }
@@ -77,7 +80,7 @@ class MyPromise{
         setTimeout(function () {
           try {
             var x = onRejected(self.value);
-            resolutionProcedure(promise2, x, resolve, reject);
+            this.resolutionProcedure(promise2, x, resolve, reject);
           } catch (reason) {
             reject(reason);
           }
@@ -89,7 +92,7 @@ class MyPromise{
         self.resolvedCallbacks.push(function () {
           try {
             var x = onResolved(self.value);
-            resolutionProcedure(promise2, x, resolve, reject);
+            this.resolutionProcedure(promise2, x, resolve, reject);
           } catch (r) {
             reject(r);
           }
@@ -97,7 +100,7 @@ class MyPromise{
         self.rejectedCallbacks.push(function () {
           try {
             var x = onRejected(self.value);
-            resolutionProcedure(promise2, x, resolve, reject);
+            this.resolutionProcedure(promise2, x, resolve, reject);
           } catch (r) {
             reject(r);
           }
@@ -105,84 +108,84 @@ class MyPromise{
       }));
     }
   }
-}
-function resolutionProcedure(promise2, x, resolve, reject) {
-  if (promise2 === x) {
-    return reject(new TypeError("Error"));
-  }
-  if (x instanceof MyPromise) {
-    if (x.currentState === PENDING) {
-      x.then(function (value) {
-        // 再次调用该函数是为了确认 x resolve 的
-        // 参数是什么类型，如果是基本类型就再次 resolve
-        // 把值传给下个 then
-        resolutionProcedure(promise2, value, resolve, reject);
-      }, reject);
-    } else {
-      x.then(resolve, reject);
+  resolutionProcedure(promise2, x, resolve, reject) {
+    if (promise2 === x) {
+      return reject(new TypeError("Error"));
     }
-    return;
-  }
-  // 规范 2.3.3.3.3: reject 或者 resolve 其中一个执行过得话，忽略其他的
-  let called = false;
-  // 规范 2.3.3:判断 x 是否为对象或者函数
-  if (x !== null && (typeof x === "object" || typeof x === "function")) {
-    // 规范 2.3.3.2:如果不能取出 then，就 reject
-    try {
-      let then = x.then;
-      // 如果 then 是函数，调用 x.then
-      if (typeof then === "function") {
-        then.call(
-          x,
-          y => {
-            if (called) return;
-            called = true;
-            resolutionProcedure(promise2, y, resolve, reject);
-          },
-          e => {
-            if (called) return;
-            called = true;
-            reject(e);
-          }
-        );
+    if (x instanceof MyPromise) {
+      if (x.currentState === PENDING) {
+        x.then(function (value) {
+          // 再次调用该函数是为了确认 x resolve 的
+          // 参数是什么类型，如果是基本类型就再次 resolve
+          // 把值传给下个 then
+          this.resolutionProcedure(promise2, value, resolve, reject);
+        }, reject);
       } else {
-        resolve(x);
+        x.then(resolve, reject);
       }
-    } catch (e) {
-      if (called) return;
-      called = true;
-      reject(e);
+      return;
     }
-  } else {
-    // 规范 2.3.4，x 为基本类型
-    resolve(x);
+    // 规范 2.3.3.3.3: reject 或者 resolve 其中一个执行过得话，忽略其他的
+    let called = false;
+    // 规范 2.3.3:判断 x 是否为对象或者函数
+    if (x !== null && (typeof x === "object" || typeof x === "function")) {
+      // 规范 2.3.3.2:如果不能取出 then，就 reject
+      try {
+        let then = x.then;
+        // 如果 then 是函数，调用 x.then
+        if (typeof then === "function") {
+          then.call(
+            x,
+            y => {
+              if (called) return;
+              called = true;
+              this.resolutionProcedure(promise2, y, resolve, reject);
+            },
+            e => {
+              if (called) return;
+              called = true;
+              reject(e);
+            }
+          );
+        } else {
+          resolve(x);
+        }
+      } catch (e) {
+        if (called) return;
+        called = true;
+        reject(e);
+      }
+    } else {
+      // 规范 2.3.4，x 为基本类型
+      resolve(x);
+    }
   }
 }
 
 
-const axios = function(url) {
-  const promise = new MyPromise(function(resolve, reject){
-      const handler = function() {
-          if (this.readyState !== 4) {
-              return;
-          }
-          if (this.status === 200) {
-              resolve(this.response);
-          } else {
-              reject(new Error(this.statusText));
-          }
-      };
-      const client = new XMLHttpRequest();
-      client.open("GET", url);
-      client.onreadystatechange = handler;
-      client.responseType = "json";
-      client.setRequestHeader("Accept", "application/json");
-      client.send(null);
+const axios = function (url) {
+  const promise = new MyPromise(function (resolve, reject) {
+    const handler = function () {
+      if (this.readyState !== 4) {
+        return;
+      }
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    };
+    const client = new XMLHttpRequest();
+    client.open("GET", url);
+    client.onreadystatechange = handler;
+    client.responseType = "json";
+    client.setRequestHeader("Accept", "application/json");
+    client.send(null);
   });
   return promise;
 };
-axios("https://ipinfo.io").then(function(json) {
-  console.log('Contents: ' + json);
-}, function(error) {
+axios("https://ipinfo.io").then(function (json) {
+  console.log(json);
+}, function (error) {
   console.error('出错了', error);
 });
--- sql/mysql.sql
+++ sql/mysql.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS `topic_collect`;
 --创建表
 CREATE TABLE `topic_collect` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户id',
-  `topic_id` int(10) NOT NULL COMMENT '帖子id',
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户id',
+  `content` int(10) NOT NULL COMMENT '帖子id',
   `status` int(1) NOT NULL COMMENT '状态1:有效,0:无效',
   `created_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -18,3 +18,20 @@ CREATE TABLE `topic_collect` (
 truncate table `topic_collect`;
 --更改表名
 alter table `topic_collect` rename to `topic_collects`;
+--查询元数据
+USE information_schema;
+SELECT
+    C.TABLE_NAME AS '表名' ,
+    T.TABLE_COMMENT AS '表说明' ,
+    C.COLUMN_NAME AS '字段名',
+    C.COLUMN_TYPE AS '数据类型',
+    C.IS_NULLABLE AS '允许为空',
+    C.EXTRA AS '主键',
+    C.COLUMN_COMMENT AS '字段说明'
+FROM COLUMNS C
+INNER JOIN TABLES T ON C.TABLE_SCHEMA = T.TABLE_SCHEMA AND C.TABLE_NAME = T.TABLE_NAME
+WHERE T.TABLE_SCHEMA = 'xboot' and T.TABLE_NAME='t_user'
+--导出数据
+mysqldump -h[远程数据库ip] -P[数据库端口号] -u[用户名] -p[密码] default-character-set="数据库编码" >db.sql;
+--查询日期
+select * from dws_adorawe_spu_daily T where T.track_date between '2021-03-21 00:00:00' and '2021-03-28 23:59:59';
