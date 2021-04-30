c3446432ddbe7e2d8d3c440cb86b8be8e423404d
chenxin <chenxin@starlinke.com>
2021-04-30 18:16:22 +0800

    update

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -37,5 +37,23 @@ INNER JOIN TABLES T ON C.TABLE_SCHEMA = T.TABLE_SCHEMA AND C.TABLE_NAME = T.TABL
 WHERE T.TABLE_SCHEMA = 'xboot' and T.TABLE_NAME='t_user'
 --导出数据
 mysqldump -h[远程数据库ip] -P[数据库端口号] -u[用户名] -p[密码] default-character-set="数据库编码" >db.sql;
+
+
 --查询日期
 select * from dws_adorawe_spu_daily T where T.track_date between '2021-03-21 00:00:00' and '2021-03-28 23:59:59';
+--按周分组，返回周一
+select 
+  vendor as name, IFNULL(roi,0) as y,
+  DATE_ADD('1900-01-01',INTERVAL FLOOR(DATEDIFF(bizdate, '1900-01-01') / 7) * 7 DAY) AS x
+  from ads_report_daily_report ardr
+  where ardr.bizdate > "2021-04-21"
+  and ardr.bizdate < "2021-04-29"
+group by name, x
+--按周分组
+select 
+  vendor as name, IFNULL(roi,0) as y,
+  DATE_FORMAT(bizdate,"%Y-%u") as x
+  from ads_report_daily_report ardr
+  where ardr.bizdate > "2021-04-21"
+  and ardr.bizdate < "2021-04-29"
+group by name, x
