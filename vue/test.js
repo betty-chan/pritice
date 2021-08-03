6c06c3b0b0f84a01a1f6d07a80cc45af56f1e5ba
chenxin <chenxin@starlinke.com>
2021-08-03 18:00:12 +0800

    update

--- vue/test.js
+++ vue/test.js
@@ -1,34 +1,4 @@
-function Observer(obj, key, value) {
-    var terrace = new Terrace();
-    if (Object.prototype.toString.call(value) == '[object Object]') {
-        Object.keys(value).forEach(function (key) {
-            new Observer(value, key, value[key])
-        })
-    };
-    Object.defineProperty(obj, key, {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            if (Terrace.target) {
-                terrace.addSub(Terrace.target);
-            };
-            return value;
-        },
-        set: function (newVal) {
-            value = newVal;
-            terrace.notify();
-        }
-    })
-}
-function Watcher(fn) {
-    this.update = function () {
-        Terrace.target = this;
-        fn();
-        Terrace.target = null;
-    }
-    this.update();
-}
-function Terrace() {
+function Dep() {
     this.subs = [];
     this.addSub = function (watcher) {
         this.subs.push(watcher);
@@ -39,3 +9,36 @@ function Terrace() {
         });
     }
 }
+function Observer(obj, key, value) {
+    var dep = new Dep();
+    if (Object.prototype.toString.call(value) == '[object Object]') {
+        Object.keys(value).forEach(function (key) {
+            new Observer(value, key, value[key])
+        })
+    };
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            if (Dep.target) {
+                dep.addSub(Dep.target);
+            };
+            return value;
+        },
+        set: function (newVal) {
+            value = newVal;
+            dep.notify();
+        }
+    })
+}
+function Watcher(fn) {
+    this.update = function () {
+        Dep.target = this;
+        fn();
+        Dep.target = null;
+    }
+    this.update();
+}
+
+//一个全局变量
+Dep.target = null;
