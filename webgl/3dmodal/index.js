71a5a19add490781d31214dc354ef3917ab7468c
chenxin <chenxin@starlinke.com>
2021-08-23 18:20:41 +0800

    update

--- webgl/3dmodal/index.js
+++ webgl/3dmodal/index.js
@@ -8,17 +8,26 @@ function init() {
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
     camera.position.z = 1;
     scene = new THREE.Scene();
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    material = new THREE.MeshStandardMaterial();
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    light = new THREE.AmbientLight(0xffffff);
+    // geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    // material = new THREE.MeshStandardMaterial();
+    // mesh = new THREE.Mesh(geometry, material);
+    // scene.add(mesh);
+    light = new THREE.AmbientLight(0x121212);
     scene.add(light);
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animation);
     document.body.appendChild(renderer.domElement);
     document.addEventListener('mousedown', mousedownFun, false);
+}
+function initModal() {
+    const loader = new GLTFLoader();
+    loader.load('./loungeSofaLong.glb', function (gltf) {
+        scene.add(gltf.scene);
+        renderer.render(scene, camera);
+    }, undefined, function (error) {
+        console.error(error);
+    });
 }
 function animation(time) {
     // mesh.rotation.x = time / 2000;
@@ -30,8 +39,6 @@ function mousedownFun(event) {
     document.addEventListener("mouseup", mouseupFun);
 }
 function mousemoveFun(event) {
-    mesh.rotation.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mesh.rotation.y = (event.clientY / window.innerHeight) * 2 + 1;
 }
 function mouseupFun() {
     document.removeEventListener('mousemove', mousemoveFun);
