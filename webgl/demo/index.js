52f947aff85ab91d0944b8841769274d9711604a
betty_chan <betty_chan@qq.com>
2021-12-14 19:36:43 +0800

    update

--- webgl/demo/index.js
+++ webgl/demo/index.js
@@ -1,13 +1,17 @@
 let camera, scene, renderer;
 let geometry, material, mesh;
-let light;
-
+let light, controls, face, params;
 let intersects;
 
 init();
 
 function init() {
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.01,
+        10
+    );
     camera.position.z = 1;
     scene = new THREE.Scene();
     geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
@@ -17,9 +21,9 @@ function init() {
         new THREE.MeshStandardMaterial(),
         new THREE.MeshStandardMaterial(),
         new THREE.MeshStandardMaterial(),
-        new THREE.MeshStandardMaterial()
+        new THREE.MeshStandardMaterial(),
     ];
-    var faceMaterial = new THREE.MeshFaceMaterial( materials );
+    var faceMaterial = new THREE.MeshFaceMaterial(materials);
     mesh = new THREE.Mesh(geometry, faceMaterial);
     scene.add(mesh);
     light = new THREE.AmbientLight(0xffffff);
@@ -27,50 +31,85 @@ function init() {
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animation);
-    
+
     document.body.appendChild(renderer.domElement);
-    document.addEventListener('mousedown', mousedownFun, false);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
     hoverFace(renderer.domElement);
 }
 function animation(time) {
-    // mesh.rotation.x = time / 2000;
-    // mesh.rotation.y = time / 1000;
     renderer.render(scene, camera);
 }
+let start = [0, 0],
+    length = 1,
+    x0 = 1,
+    y0 = 1,
+    z0 = 1;
 function mousedownFun(event) {
-    document.addEventListener("mousemove", mousemoveFun, false);
-    document.addEventListener("mouseup", mouseupFun);
+    start[0] = event.clientX;
+    start[1] = event.clientY;
+    document.addEventListener('mousemove', mousemoveFun, false);
+    document.addEventListener('mouseup', mouseupFun);
 }
 function mousemoveFun(event) {
-    mesh.rotation.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mesh.rotation.y = (event.clientY / window.innerHeight) * 2 + 1;
+    let w = (event.clientX - start[0]) / window.innerWidth / 50;
+    let l = (event.clientY - start[1]) / window.innerHeight / 50;
+    length = Math.sqrt(w * w + l * l) + 1;
+    params.multiplyScalar(length);
+    let { x, y, z } = { ...params };
+    x0 += Math.abs(x);
+    y0 += Math.abs(y);
+    z0 += Math.abs(z);
+    mesh.scale.set(x0, y0, z0);
 }
 function mouseupFun() {
+    document.removeEventListener('mousedown', mousedownFun);
     document.removeEventListener('mousemove', mousemoveFun);
     document.removeEventListener('mouseup', mouseupFun);
+    start = [0, 0];
 }
 function hoverFace(dom) {
-    dom.addEventListener("mousemove", mouseenterFun);
+    dom.addEventListener('mousemove', mouseenterFun);
+}
+function getFace(ver, face) {
+    let point = [face.a, face.b, face.c];
+    let vertices = [];
+    point.forEach((item) => {
+        vertices.push(ver[item]);
+    });
+    let result1 = vertices[0].clone();
+    let result2 = vertices[0].clone();
+    result1.sub(vertices[1]);
+    result2.sub(vertices[2]);
+    result1.cross(result2);
+    return result1;
 }
 //实现hover几何体变色
 function mouseenterFun(event) {
     let raycaster = new THREE.Raycaster();
     let mouse = new THREE.Vector2();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = 1-(event.clientY / window.innerHeight) * 2;
+    mouse.y = 1 - (event.clientY / window.innerHeight) * 2;
     raycaster.setFromCamera(mouse, camera);
     intersects = raycaster.intersectObjects(scene.children);
+    document.removeEventListener('mousedown', mousedownFun);
     //将所有的相交的模型的颜色设置为红色
     for (var i = 0; i < intersects.length; i++) {
         let materialIndex = intersects[i].face.materialIndex;
+        params = getFace(
+            intersects[i].object.geometry.vertices,
+            intersects[i].face
+        );
         intersects[i].object.material[materialIndex].color.set(0xff0000);
-        mesh.material.forEach((item,key)=>{
-            item.color.set(key==materialIndex?0xff0000:0xffffff);
-        })
+        mesh.material.forEach((item, key) => {
+            item.color.set(key == materialIndex ? 0xff0000 : 0xffffff);
+        });
+        controls.enableRotate = false;
+        document.addEventListener('mousedown', mousedownFun, false);
     }
-    if(!intersects ||!intersects.length){
-        mesh.material.forEach((item)=>{
+    if (!intersects || !intersects.length) {
+        mesh.material.forEach((item) => {
             item.color.set(0xffffff);
-        })
+        });
+        controls.enableRotate = true;
     }
 }
---
+++ webgl/demo1/index.js
@@ -0,0 +1,88 @@
+let camera, scene, renderer;
+let geometry, material, mesh;
+let light;
+
+let intersects;
+
+init();
+
+function init() {
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera.position.z = 1;
+    scene = new THREE.Scene();
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    var materials = [
+        new THREE.MeshStandardMaterial(),
+        new THREE.MeshStandardMaterial(),
+        new THREE.MeshStandardMaterial(),
+        new THREE.MeshStandardMaterial(),
+        new THREE.MeshStandardMaterial(),
+        new THREE.MeshStandardMaterial()
+    ];
+    var faceMaterial = new THREE.MeshFaceMaterial( materials );
+    mesh = new THREE.Mesh(geometry, faceMaterial);
+    scene.add(mesh);
+    light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animation);
+    
+    document.body.appendChild(renderer.domElement);
+    hoverFace(renderer.domElement);
+    initDragControls();
+}
+function initDragControls() {
+    var transformControls = new THREE.TransformControls(camera, renderer.domElement);
+    scene.add(transformControls);
+    var objects = [];
+    for (let i = 0; i < scene.children.length; i++) {
+        if (scene.children[i].isMesh) {
+            objects.push(scene.children[i]);
+        }
+    }
+    var dragControls = new THREE.DragControls(objects, camera, renderer.domElement);
+    // dragControls.addEventListener('hoveron', function (event) {
+    //     transformControls.attach(event.object);
+    // });
+    dragControls.addEventListener('dragstart', function (event) {
+        event.object.material.forEach((item)=>{
+            item.emissive.set(0xaaaaaa);
+        })
+    });
+    dragControls.addEventListener('dragend', function (event) {
+        event.object.material.forEach((item)=>{
+            item.emissive.set(0x000000);
+        })
+    });
+}
+function animation(time) {
+    // mesh.rotation.x = time / 2000;
+    // mesh.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+function hoverFace(dom) {
+    dom.addEventListener("mousemove", mouseenterFun);
+}
+//实现hover几何体变色
+function mouseenterFun(event) {
+    let raycaster = new THREE.Raycaster();
+    let mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = 1-(event.clientY / window.innerHeight) * 2;
+    raycaster.setFromCamera(mouse, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    //将所有的相交的模型的颜色设置为红色
+    for (var i = 0; i < intersects.length; i++) {
+        let materialIndex = intersects[i].face.materialIndex;
+        intersects[i].object.material[materialIndex].color.set(0xff0000);
+        mesh.material.forEach((item,key)=>{
+            item.color.set(key==materialIndex?0xff0000:0xffffff);
+        })
+    }
+    if(!intersects ||!intersects.length){
+        mesh.material.forEach((item)=>{
+            item.color.set(0xffffff);
+        })
+    }
+}
