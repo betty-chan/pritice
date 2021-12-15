214eedb0258e6864bb62473993f8fdf58d06f601
betty_chan <betty_chan@qq.com>
2021-12-15 18:40:14 +0800

    update

--- webgl/demo/index.js
+++ webgl/demo/index.js
@@ -2,6 +2,7 @@ let camera, scene, renderer;
 let geometry, material, mesh;
 let light, controls, face, params;
 let intersects;
+let oldFace = null, loading = false;
 
 init();
 
@@ -14,6 +15,21 @@ function init() {
     );
     camera.position.z = 1;
     scene = new THREE.Scene();
+    mesh = addBoxGeometry();
+    scene.add(mesh);
+    addLight();
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animation);
+    document.body.appendChild(renderer.domElement);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    startHoverFace(renderer.domElement);
+}
+function addLight() {
+    light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
+function addBoxGeometry() {
     geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
     var materials = [
         new THREE.MeshStandardMaterial(),
@@ -24,53 +40,47 @@ function init() {
         new THREE.MeshStandardMaterial(),
     ];
     var faceMaterial = new THREE.MeshFaceMaterial(materials);
-    mesh = new THREE.Mesh(geometry, faceMaterial);
-    scene.add(mesh);
-    light = new THREE.AmbientLight(0xffffff);
-    scene.add(light);
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setAnimationLoop(animation);
-
-    document.body.appendChild(renderer.domElement);
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    hoverFace(renderer.domElement);
+    return new THREE.Mesh(geometry, faceMaterial);
 }
-function animation(time) {
+function animation() {
     renderer.render(scene, camera);
 }
-let start = [0, 0],
-    length = 1,
-    x0 = 1,
-    y0 = 1,
-    z0 = 1;
-function mousedownFun(event) {
-    start[0] = event.clientX;
-    start[1] = event.clientY;
+function mousedownFun() {
+    loading = true;
     document.addEventListener('mousemove', mousemoveFun, false);
     document.addEventListener('mouseup', mouseupFun);
 }
 function mousemoveFun(event) {
-    let w = (event.clientX - start[0]) / window.innerWidth / 50;
-    let l = (event.clientY - start[1]) / window.innerHeight / 50;
-    length = Math.sqrt(w * w + l * l) + 1;
-    params.multiplyScalar(length);
-    let { x, y, z } = { ...params };
-    x0 += Math.abs(x);
-    y0 += Math.abs(y);
-    z0 += Math.abs(z);
-    mesh.scale.set(x0, y0, z0);
+    let mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = 1 - (event.clientY / window.innerHeight) * 2;
+    let vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
+    vector.unproject(camera).sub(camera.position).normalize();
+    let length = vector.length() / 2;
+    if (params) {
+        params.multiplyScalar(length);
+        let { x, y, z } = { ...params };
+        let x0 = Math.abs(x) + mesh.scale.x;
+        let y0 = Math.abs(y) + mesh.scale.y;
+        let z0 = Math.abs(z) + mesh.scale.z;
+        mesh.scale.set(x0, y0, z0);
+        mesh.position.x += x * 0.2;
+        mesh.position.y += y * 0.2;
+        mesh.position.z += z * 0.2;
+    }
 }
 function mouseupFun() {
     document.removeEventListener('mousedown', mousedownFun);
     document.removeEventListener('mousemove', mousemoveFun);
     document.removeEventListener('mouseup', mouseupFun);
-    start = [0, 0];
+    loading = false;
+    params = null;
 }
-function hoverFace(dom) {
-    dom.addEventListener('mousemove', mouseenterFun);
-}
-function getFace(ver, face) {
+function getDirection(ver, face) {
+    if (oldFace && face !== oldFace && loading == true) {
+        return;
+    }
+    oldFace = face;
     let point = [face.a, face.b, face.c];
     let vertices = [];
     point.forEach((item) => {
@@ -81,35 +91,42 @@ function getFace(ver, face) {
     result1.sub(vertices[1]);
     result2.sub(vertices[2]);
     result1.cross(result2);
-    return result1;
+    params = result1;
+}
+function startHoverFace(dom) {
+    dom.addEventListener('mousemove', hoverFun);
 }
 //实现hover几何体变色
-function mouseenterFun(event) {
+function hoverFun(event) {
     let raycaster = new THREE.Raycaster();
     let mouse = new THREE.Vector2();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = 1 - (event.clientY / window.innerHeight) * 2;
     raycaster.setFromCamera(mouse, camera);
     intersects = raycaster.intersectObjects(scene.children);
-    document.removeEventListener('mousedown', mousedownFun);
-    //将所有的相交的模型的颜色设置为红色
-    for (var i = 0; i < intersects.length; i++) {
-        let materialIndex = intersects[i].face.materialIndex;
-        params = getFace(
-            intersects[i].object.geometry.vertices,
-            intersects[i].face
-        );
-        intersects[i].object.material[materialIndex].color.set(0xff0000);
-        mesh.material.forEach((item, key) => {
-            item.color.set(key == materialIndex ? 0xff0000 : 0xffffff);
-        });
-        controls.enableRotate = false;
-        document.addEventListener('mousedown', mousedownFun, false);
-    }
     if (!intersects || !intersects.length) {
-        mesh.material.forEach((item) => {
-            item.color.set(0xffffff);
-        });
-        controls.enableRotate = true;
+        //清空红色
+        if (!loading) {
+            mesh.material.forEach((item) => {
+                item.color.set(0xffffff);
+            });
+            controls.enableRotate = true;
+            document.removeEventListener('mousedown', mousedownFun);
+        }
+    } else {
+        getDirection(
+            intersects[0].object.geometry.vertices,
+            intersects[0].face
+        );
+        //设置选中face为红色
+        if (!loading) {
+            let materialIndex = intersects[0].face.materialIndex;
+            intersects[0].object.material[materialIndex].color.set(0xff0000);
+            mesh.material.forEach((item, key) => {
+                item.color.set(key == materialIndex ? 0xff0000 : 0xffffff);
+            });
+            controls.enableRotate = false;
+            document.addEventListener('mousedown', mousedownFun, false);
+        }
     }
 }
